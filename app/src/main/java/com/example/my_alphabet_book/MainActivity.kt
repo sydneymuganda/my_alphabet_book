8e5fd39d4b7e8a7359981b600520097e41e77508
sydney_muganda <sydneymuganda@yahoo.com>
2022-09-15 18:34:02 +0200

    added shared preferences to allow app to maintain its state on closure

--- app/src/main/java/com/example/my_alphabet_book/MainActivity.kt
+++ app/src/main/java/com/example/my_alphabet_book/MainActivity.kt
@@ -1,11 +1,163 @@
 package com.example.my_alphabet_book
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    fun ToA(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",0)
+        startActivity(intent)
+    }
+    fun ToB(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",1)
+        startActivity(intent)
+    }
+    fun ToC(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",2)
+        startActivity(intent)
+    }
+    fun ToD(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",3)
+        startActivity(intent)
+    }
+    fun ToE(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",4)
+        startActivity(intent)
+    }
+    fun ToF(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",5)
+        startActivity(intent)
+    }
+    fun ToG(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",6)
+        startActivity(intent)
+    }
+    fun ToH(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",7)
+        startActivity(intent)
+    }
+    fun ToI(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",8)
+        startActivity(intent)
+    }
+    fun ToJ(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",9)
+        startActivity(intent)
+    }
+    fun ToK(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",10)
+        startActivity(intent)
+    }
+    fun ToL(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",11)
+        startActivity(intent)
+    }
+    fun ToM(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",12)
+        startActivity(intent)
+    }
+    fun ToN(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",13)
+        startActivity(intent)
+    }
+    fun ToO(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",14)
+        startActivity(intent)
+    }
+    fun ToP(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",15)
+        startActivity(intent)
+    }
+    fun ToQ(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",16)
+        startActivity(intent)
+    }
+    fun ToR(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",17)
+        startActivity(intent)
+    }
+    fun ToS(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",18)
+        startActivity(intent)
+    }
+    fun ToT(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",19)
+        startActivity(intent)
+    }
+    fun ToU(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",20)
+        startActivity(intent)
+    }
+    fun ToV(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",21)
+        startActivity(intent)
+    }
+    fun ToW(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",22)
+        startActivity(intent)
+    }
+    fun ToX(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",23)
+        startActivity(intent)
+    }
+    fun ToY(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",24)
+        startActivity(intent)
+    }
+    fun ToZ(v: View){
+        val intent = Intent(this,flipper::class.java)
+        intent.putExtra("flip",25)
+        startActivity(intent)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (application.getSharedPreferences("my_data",Context.MODE_PRIVATE)!=null) {
+            var sp = applicationContext.getSharedPreferences("my_data", Context.MODE_PRIVATE)
+            sp.edit().clear()
+            sp.edit().commit()
+        }
+    }
+
 }
--- app/src/main/java/com/example/my_alphabet_book/flipper.kt
+++ app/src/main/java/com/example/my_alphabet_book/flipper.kt
@@ -1,15 +1,21 @@
 package com.example.my_alphabet_book
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.ViewFlipper
 
 class flipper : AppCompatActivity() {
+    lateinit var sp: SharedPreferences
     lateinit var viewflipper: ViewFlipper
     lateinit var images:ArrayList<ImageView>
     lateinit var imageView: ImageView
-
+    var c:Int = 0
+    lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.image_flipper)
@@ -20,84 +26,169 @@ class flipper : AppCompatActivity() {
         imageView.setImageResource(R.drawable.slide01)
         viewflipper.addView(imageView) //added image 1
 
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide02)
         viewflipper.addView(imageView)//added image 2
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide03)
         viewflipper.addView(imageView) //added image 3
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide04)
         viewflipper.addView(imageView) //added image 4
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide05)
         viewflipper.addView(imageView) //added image 5
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide06)
         viewflipper.addView(imageView) //added image 6
 
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide07)
         viewflipper.addView(imageView) //added image 7
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide08)
         viewflipper.addView(imageView) //added image 8
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide09)
         viewflipper.addView(imageView) // //added image 9
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide10)
         viewflipper.addView(imageView) //added image 10
 
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide11)
         viewflipper.addView(imageView) //added image 11
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide12)
         viewflipper.addView(imageView) //added image 12
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide13)
         viewflipper.addView(imageView) //added image 13
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide14)
         viewflipper.addView(imageView) //added image 14
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide15)
         viewflipper.addView(imageView) //added image 15
 
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide16)
         viewflipper.addView(imageView) //added image 16
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide17)
         viewflipper.addView(imageView) //added image 17
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide18)
         viewflipper.addView(imageView) //added image 18
 
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide19)
         viewflipper.addView(imageView) //added image 19
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide20)
         viewflipper.addView(imageView) //added image 20
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide21)
         viewflipper.addView(imageView) //added image 21
 
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide22)
         viewflipper.addView(imageView) //added image 22
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide23)
         viewflipper.addView(imageView) //added image 23
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide24)
         viewflipper.addView(imageView)//added image 24
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide25)
         viewflipper.addView(imageView)//added image 25
 
+
+        imageView =  ImageView(this)
         imageView.setImageResource(R.drawable.slide26)
         viewflipper.addView(imageView)//added image 26
+
+        sp=getSharedPreferences("my_data", Context.MODE_PRIVATE)
+         editor=sp.edit()
+         c=intent.extras?.get("flip")as Int
+
+         specificView(sp.getInt("saved_view",c))
 
 
 
         //viewflipper.addView()
        // val
     }
+    fun PreviousView(v: View){
+     viewflipper.showPrevious()
+    }
+    fun NextView(v: View){
+        viewflipper.showNext()
+    }
+    fun specificView(c:Int){
+        viewflipper.setDisplayedChild(c)
+    }
+    fun toFirstPage(v: View){
+        specificView(0)
+
+    }
+    fun toLastPage(v: View){
+
+        specificView(25)
+    }
+    fun backToHome(v: View){
+        val intent = Intent(this,MainActivity::class.java)
+
+        startActivity(intent)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        var current_view=viewflipper.indexOfChild(viewflipper.currentView)
+
+
+        editor.putInt("saved_view",current_view)
+        editor.commit()
+    }
+
+//    override fun onResume() {
+//        super.onResume()
+//    }
 }
